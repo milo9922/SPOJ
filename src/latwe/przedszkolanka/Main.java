d5be9ae0edbd2e5f651ed1be4b1ad9007516b0f3
milo9922 <52604121+milo9922@users.noreply.github.com>
2019-10-23 16:20:13 +0200

    add przedszkolanka working solution

--- src/latwe/przedszkolanka/Main.java
+++ src/latwe/przedszkolanka/Main.java
@@ -16,52 +16,33 @@ public class Main {
         for (int i = 0; i <= n; i++) {
             String children = scanner.nextLine();
             char[] childrenArray = children.toCharArray();
-            int num1;
-            int num2;
+            int num1, num2;
+
             if (children.length() > 0) {
-                switch (children.length()) {
-
-                    case 3: {
-                        String tempNum1 = Character.toString(childrenArray[0]);
-                        String tempNum2 = Character.toString(childrenArray[2]);
-                        num1 = Integer.parseInt(tempNum1);
-                        num2 = Integer.parseInt(tempNum2);
+                switch (childrenArray.length) {
+                    case 3:
+                        num1 = Integer.parseInt(Character.toString(childrenArray[0]));
+                        num2 = Integer.parseInt(Character.toString(childrenArray[2]));
                         break;
-                    }
 
-                    case 4: {
+                    case 4:
                         if (childrenArray[1] == ' ') {
-                            String tempNum1 = Character.toString(childrenArray[0]);
-                            String tempNum2 = Character.toString(childrenArray[2]) + Character.toString(childrenArray[3]);
-                            num1 = Integer.parseInt(tempNum1);
-                            num2 = Integer.parseInt(tempNum2);
+                            num1 = Integer.parseInt(Character.toString(childrenArray[0]));
+                            num2 = Integer.parseInt(Character.toString(childrenArray[2]) + Character.toString(childrenArray[3]));
                         } else {
-                            String tempNum1 = Character.toString(childrenArray[0]) + Character.toString(childrenArray[1]);
-                            String tempNum2 = Character.toString(childrenArray[3]);
-                            num1 = Integer.parseInt(tempNum1);
-                            num2 = Integer.parseInt(tempNum2);
+                            num1 = Integer.parseInt(Character.toString(childrenArray[0]) + Character.toString(childrenArray[1]));
+                            num2 = Integer.parseInt(Character.toString(childrenArray[3]));
                         }
                         break;
-                    }
 
-                    case 5: {
-                        String tempNum1 = Character.toString(childrenArray[0]) + Character.toString(childrenArray[1]);
-                        String tempNum2 = Character.toString(childrenArray[3]) + Character.toString(childrenArray[4]);
-                        num1 = Integer.parseInt(tempNum1);
-                        num2 = Integer.parseInt(tempNum2);
-                        break;
-                    }
-                    default: {
-                        num1 = 1;
-                        num2 = 1;
-                        break;
-                    }
+                    case 5:
+                        num1 = Integer.parseInt(Character.toString(childrenArray[0]) + Character.toString(childrenArray[1]));
+                        num2 = Integer.parseInt(Character.toString(childrenArray[3]) + Character.toString(childrenArray[4]));
+
+                        System.out.println(num1 / commonMultiple(num1, num2) * num2);
                 }
-                System.out.println(num1 / commonMultiple(num1, num2) * num2);
             }
         }
-
-
     }
 
     private static int commonMultiple(int num1, int num2) {
