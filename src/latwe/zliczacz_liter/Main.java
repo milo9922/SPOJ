af8537984b7113e7536416deecc7b13719f25641
milo9922 <52604121+milo9922@users.noreply.github.com>
2020-01-27 12:18:44 +0100

    moved code to 2 additional methods (instead of everything in main)

--- src/latwe/zliczacz_liter/Main.java
+++ src/latwe/zliczacz_liter/Main.java
@@ -11,35 +11,43 @@ public class Main {
             int numberOfRows = scanner.nextInt();
             String text = "";
             scanner.nextLine();
+
             for (int i = 0; i < numberOfRows; i++) {
                 text += scanner.nextLine();
             }
+            printResult(getAlphabet(text));
 
-            int[] alphabet = new int[52];
-            for (int i = 0; i < text.length(); i++) {
-                char currentLetter = text.charAt(i);
-                int numericValueOfLetter = (int) currentLetter;
-
-                if (Character.isLowerCase(currentLetter)) {
-                    alphabet[numericValueOfLetter - 97]++;
-                } else if (Character.isUpperCase(currentLetter)) {
-                    alphabet[numericValueOfLetter - 65 + 26]++;
-                }
-
-            }
-
-            for (int i = 0; i < alphabet.length; i++) {
-                if (alphabet[i] != 0) {
-                    if (i < 26) {
-                        int numericValueOfLetter = i + 97;
-                        System.out.println(((char) numericValueOfLetter) + " " + alphabet[i]);
-                    } else {
-                        int numericValueOfLetter = i + 39;
-                        System.out.println((char) numericValueOfLetter + " " + alphabet[i]);
-                    }
-                }
-            }
         } catch (Exception ignore) {
+        }
+    }
+
+    private static int[] getAlphabet(String text) {
+        int[] alphabet = new int[52];
+        for (int i = 0; i < text.length(); i++) {
+            char currentLetter = text.charAt(i);
+            int numericValueOfLetter = (int) currentLetter;
+
+            if (Character.isLowerCase(currentLetter)) {
+                alphabet[numericValueOfLetter - 97]++;
+            } else if (Character.isUpperCase(currentLetter)) {
+                alphabet[numericValueOfLetter - 65 + 26]++;
+            }
+
+        }
+        return alphabet;
+    }
+
+    private static void printResult(int[] alphabet) {
+        for (int i = 0; i < alphabet.length; i++) {
+            if (alphabet[i] != 0) {
+                if (i < 26) {
+                    int numericValueOfLetter = i + 97;
+                    System.out.println(((char) numericValueOfLetter) + " " + alphabet[i]);
+                } else {
+                    int numericValueOfLetter = i + 39;
+                    System.out.println((char) numericValueOfLetter + " " + alphabet[i]);
+                }
+            }
         }
     }
 
